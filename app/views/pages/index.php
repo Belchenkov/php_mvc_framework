daba10549eae4630a156876612d681706d80cf4c
Aleksey Belchenkov <u608110@gmail.com>
2019-02-01 07:10:56 +0300

    Finish Core

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,9 +1,4 @@
 <?php require_once APPROOT . '/views/inc/header.php' ?>
     <h1><?= $data['title'] ?></h1>
 
-    <ul>
-        <?php foreach ($data['posts'] as $post) : ?>
-            <li><?= $post->title ?></li>
-        <?php endforeach; ?>
-    </ul>
 <?php require_once APPROOT . '/views/inc/footer.php' ?>
