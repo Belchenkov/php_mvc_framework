eb582a3acdcf6d66527a4f80885f45a18d460ff8
Aleksey Belchenkov <u608110@gmail.com>
2019-02-01 07:03:46 +0300

    Fetch Data

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,9 @@
 <?php require_once APPROOT . '/views/inc/header.php' ?>
-<h1><?= $data['title'] ?></h1>
+    <h1><?= $data['title'] ?></h1>
+
+    <ul>
+        <?php foreach ($data['posts'] as $post) : ?>
+            <li><?= $post->title ?></li>
+        <?php endforeach; ?>
+    </ul>
 <?php require_once APPROOT . '/views/inc/footer.php' ?>
