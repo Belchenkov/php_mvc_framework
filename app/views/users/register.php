c073791cc1b7da17436cedfdfeef746b67e889bb
Aleksey Belchenkov <u608110@gmail.com>
2019-02-02 07:52:11 +0300

    Creating The Users Controller

---
+++ app/views/users/register.php
@@ -0,0 +1,12 @@
+<?php require_once APPROOT . '/views/inc/header.php'; ?>
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card card-body bg-primary text-light mt-5">
+                <h2>
+                    <img src="https://img.icons8.com/dusk/30/000000/add-user-male.png">
+                    Create An Account
+                </h2>
+            </div>
+        </div>
+    </div>
+<?php require_once APPROOT . '/views/inc/footer.php'; ?>
