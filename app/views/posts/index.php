488b996c1f5886cc3ebd1c0a2f39c700be1b59eb
Aleksey Belchenkov <u608110@gmail.com>
2019-02-03 08:14:21 +0300

    Posts Controller

---
+++ app/views/posts/index.php
@@ -0,0 +1,12 @@
+<?php require_once APPROOT . '/views/inc/header.php' ?>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Posts</h1>
+        </div>
+        <div class="col-md-6">
+            <a href="<?=URLROOT;?>;posts/add" class="btn btn-info pull-right">
+                <img src="https://img.icons8.com/plasticine/40/000000/plus.png"> Add Post
+            </a>
+        </div>
+    </div>
+<?php require_once APPROOT . '/views/inc/footer.php' ?>
